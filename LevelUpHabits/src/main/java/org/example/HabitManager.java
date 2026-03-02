74787ae2e5c2a1e58fab18c830ec4ca09eab3b2d
Tehreem Asif <tehreem.asif63@gmail.com>
2026-03-02 14:07:24 +0100

    Adds basic functionality for adding habits and users.

--- LevelUpHabits/src/main/java/org/example/HabitManager.java
+++ LevelUpHabits/src/main/java/org/example/HabitManager.java
@@ -5,13 +5,8 @@ import java.util.Scanner;
 public class HabitManager {
     Scanner scanner = new Scanner(System.in);
     //Manages adding/removing habits, marking completion, calculating streaks and XP, and leveling users.
-    public Habit createHabit(){
-        System.out.println("Enter a habit's name to create habit: ");
-        String nameGiven = scanner.next();
-        System.out.println("Enter difficulty level from (1, 2, 3): ");
-        int diffLevel = scanner.nextInt();
+    public Habit createHabit(String nameGiven, int diffLevel){
         Habit newHabit = new Habit(nameGiven,diffLevel);
-        scanner.close();
         return newHabit;
     }
     public void addHabitToUser(User user, Habit habit){
--- LevelUpHabits/src/main/java/org/example/Main.java
+++ LevelUpHabits/src/main/java/org/example/Main.java
@@ -4,18 +4,27 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println("Starting LevelUpHabit now..../n \"Enter a user's name to create user: ");
         Scanner scanner = new Scanner(System.in);
-        String nameGiven = scanner.next();
-        User newUser = new User(nameGiven);
-        scanner.close();
-        //
+
+        System.out.println("Starting LevelUpHabit now..../n \"Enter a user's name to create user: ");
+        String userName = scanner.next();
+        User newUser = new User(userName);
+
         HabitManager habitManager = new HabitManager();
-        Habit newHabit = habitManager.createHabit();
+
+        System.out.println("Enter a habit's name to create habit: ");
+        String habitName = scanner.next();
+        System.out.println("Enter difficulty level from (1, 2, 3): ");
+        int diffLevel = scanner.nextInt();
+        Habit newHabit = habitManager.createHabit(habitName, diffLevel);
+        System.out.println(newHabit);
         //1. create a habit
         //2. mark a habit as done
         //3. view habit streaks
         //4. view xp & level
         //5. exit
+
+        scanner.close();
+
     }
 }
