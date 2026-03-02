244c47d3a008eb8694683701173d789e21ba4890
Tehreem Asif <tehreem.asif63@gmail.com>
2026-03-02 13:36:39 +0100

    Adds new methods to user class.

--- LevelUpHabits/src/main/java/org/example/User.java
+++ LevelUpHabits/src/main/java/org/example/User.java
@@ -6,11 +6,35 @@ public class User {
     private final String name;
     private int currentLevel = 0;
     private int totalXP = 0;
+
+
     private List<Habit> habits;
 
     public User(String name){
         this.name = name;
     }
 
+    public int getCurrentLevel() {
+        return currentLevel;
+    }
 
+    public int getTotalXP() {
+        return totalXP;
+    }
+
+    public List<Habit> getHabits() {
+        return habits;
+    }
+
+    public void setTotalXP(int totalXP) {
+        this.totalXP = totalXP;
+    }
+
+    public void setCurrentLevel(int currentLevel) {
+        this.currentLevel = currentLevel;
+    }
+
+    public void addHabits(Habit habit) {
+        habits.add(habit);
+    }
 }
