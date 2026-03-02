d85b58fbb8749825fe079b197b65ead4f1c7c9a7
Tehreem Asif <tehreem.asif63@gmail.com>
2026-03-02 11:39:57 +0100

    Adds habit class with some fields.

---
+++ LevelUpHabits/src/main/java/org/example/Habit.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Habit {
+    //1-10 difficulty level
+    //Streak start from 0
+    //XP reward start from 0
+    private final int difficulty = 0;
+    private final int streak = 0;
+    private final int xP = 0;
+
+
+}
